e28eefcbfa472a7bc8ae59b5b3eedd0cacc61a1b
naeemhmg <44666273+naeemhmg@users.noreply.github.com>
2019-03-26 01:18:45 -0700

    Update pairwise_seq_alignment.R

--- pairwise_seq_alignment.R
+++ pairwise_seq_alignment.R
@@ -1,18 +1,18 @@
 library(Biostrings)
 library (seqinr)
-#FASTA file having many sequences is read
-prok <- read.fasta("prok.fasta", seqtype="DNA")
+#FASTA file having many sequences is read. The name of fasta file is "seq"
+seq <- read.fasta("seq.fasta", seqtype="DNA")
 # Subseting of sequences 1 and 2 from many sequences 
-prok1 <-prok[1]
-prok2 <- prok[2]
+seq1 <-seq[1]
+seq2 <- seq[2]
 # Writing the subset sequences as individual fasta files. 
-write.fasta(sequences = prok2, names = names(prok2), as.string=FALSE, file.out = "prok2.fasta")
-write.fasta(sequences = prok1, names = names(prok1), as.string=FALSE, file.out = "prok1.fasta")
+write.fasta(sequences = seq1, names = names(seq1), as.string=FALSE, file.out = "seq1.fasta")
+write.fasta(sequences = seq2, names = names(seq2), as.string=FALSE, file.out = "seq2.fasta")
 # reading fasta files from the directory 
-read.fasta("prok1.fasta", seqtype = "DNA")
-read.fasta("prok2.fasta", seqtype = "DNA")
+read.fasta("seq1.fasta", seqtype = "DNA")
+read.fasta("seq2.fasta", seqtype = "DNA")
 # converting the Fasta files into String which can be aligned 
-prok1 =paste(prok1, collapse = "")
-prok2=paste(prok2, collapse = "")
+seq1 =paste(seq1, collapse = "")
+seq2=paste(seq2, collapse = "")
 # code for alignment
-pairwiseAlignment(pattern = prok1, subject = prok2)
+pairwiseAlignment(pattern = seq1, subject = seq2)
